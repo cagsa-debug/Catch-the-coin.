453332b01f66ee92ffac405d599c701ec8ccb2e2
cagsa-debug <cagsaganteng@gmail.com>
2026-09-12 11:15:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // =====================================
 
 const SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbwhwfmoFu97uJF7ko6GtQDAZYshu_yGUDCmNIewSSh08xHhlHNZmYEuKHIC-gxwMlBS/exec"
+    "https://script.google.com/macros/s/AKfycbwBm2L8VSthsOeF0dv4wtwJ7O907GqgjGIE0myKbut9a35hxFe76qfxZ2XuVk4sR2Cf/exec"
 
 
 // =====================================
